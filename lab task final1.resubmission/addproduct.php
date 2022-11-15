5f80e9521170ef8ca5de8f0a3c0ccdd8b1e844f8
nousheen <noushin.doe@gmail.com>
2022-11-15 21:26:02 +0600

    late upload

---
+++ lab task final1.resubmission/addproduct.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Add Product</title>
+    <body>
+        <a href="home.php">Home</a>&nbsp <a href="addProduct.php">Add Products </a> &nbsp <a href="display.php">Display Products </a>
+    <br><br>
+        <fieldset>
+    <legend>ADD PRODUCT</legend>
+         <table>
+             
+            <form method="post" action="addProductCheck.php" enctype=""> 
+                
+                <table>
+                <tr><td>Name<br><input type="text" name="name"></input></td></tr>
+                    <tr><td>Buying Price<br><input type="number" name="buyingPrice"></input></td></tr>
+                    <tr><td>Selling Price<br><input type="number" name="sellingPrice"></input></td></tr>
+                    <tr><td><hr></td></tr>
+                    <tr><td><input type="radio" name="display" value="yes"></input>Display</td></tr>
+                    <tr><td><hr></td></tr>
+                    <tr><td><input type="submit" value="Submit" ></input> </td></tr>
+</form>
+</table>
+ 
+ 
+</fieldset>
+   </body>
+</html>
---
+++ lab task final1.resubmission/display.php
@@ -0,0 +1,64 @@
+<html>
+
+ <title>Display</title>
+
+ <body>
+ <a href="home.php">Home</a>&nbsp <a href="addProduct.php">Add Products </a> &nbsp <a href="display.php">Display Products </a>
+    <br><br>
+
+<fieldset><legend>Display</legend>
+
+<table border=1>
+    <tr>
+        <th>NAME</th>
+        <th>PROFIT</th>
+        <th>EDIT</th>
+        <th>DELETE</th>
+    </tr> 
+
+
+<?php $con = mysqli_connect('localhost', 'root', '', 'product_db');
+
+$sql = "select name, selling_price-buying_price profit from products where display='yes'";
+    $result = mysqli_query($con, $sql);
+
+    
+
+
+
+    
+  
+
+ 
+
+while($data  = mysqli_fetch_assoc($result)){  ?>
+ 
+    
+  <tr>
+ <td><?php echo $data['name']?></td>      
+ <td><?php echo $data['profit']?></td>        
+ <td><a href='editProduct.php?edit=<?php  echo $data['name'];?>' class='edit_btn' >edit</a></td>    
+ <td><a href='deleteProduct.php?delete=<?php  echo $data['name'];?>' class='delete_btn' >delete</a></td>    
+ </tr>
+
+ 
+
+     
+     
+
+  
+<?php } ?>
+
+ 
+
+ 
+ 
+ </table>
+
+ 
+
+</fieldset>
+
+</body>
+
+</html>
